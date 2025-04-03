dc468c004a94ccb1ffe331ca107a5e6581a1b01c
Arkashish Sutradhar <arkashishdhar1@gmail.com>
2025-04-03 22:09:47 +0530

    Added me route and also changed the path of me folder

--- src/app/api/users/me/route.ts
+++ src/app/api/users/me/route.ts
@@ -8,6 +8,7 @@ connect();
 export async function GET(request: NextRequest) {
   try {
     const userId = await getDataFromToken(request);
+    console.log(userId);
     const user = await User.findOne({ _id: userId }).select("-password");
     return NextResponse.json({
       message: "User Found",
